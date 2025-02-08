aa27838d4ac76df4b4a0b0c44664fd57c75abccf
ruslan <rusydoma@gmail.com>
2025-02-08 20:32:18 +0300

    08.02.25_Laptop_update

--- Flask/Video_Flask/L9.py
+++ Flask/Video_Flask/L9.py
@@ -2,6 +2,9 @@ from flask import Flask, g, render_template
 import sqlite3
 import os
 from fdatabase import FDatabase
+import logging, logging.config
+from utils.logger_config import dict_config
+
 
 # Config
 DATABASE = "tmp/flatsite.db"
@@ -9,21 +12,32 @@ SECRET_KEY = os.urandom(10).hex()
 DEBUG = True
 
 
+logger = logging.getLogger(__name__)
+logging.config.dictConfig(dict_config)
+logger.setLevel("DEBUG")
+# print(logger)
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 app.config.update(dict(DATABASE=os.path.join(app.root_path, "flatsite.db")))
+logger.debug("Include config success")
 
 
 def connect_db():
+    logger.debug("Start connect")
     conn = sqlite3.connect(app.config["DATABASE"])
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row # Он поддерживает доступ к результату запроса как к словарю, где ключ это имя столбца.
+                                # Так же поддерживает обращение к столбцу по индексу, итерацию по строкам запроса,
+                                # проверку на равенство и встроенную функцию len() для подсчета количества строк запроса.
     return conn
 
 
 def get_db_only_in_app():
     if not hasattr(g, "link_db"):
         g.link_db = connect_db()
+    logger.debug("Get g")
     return g.link_db
 
 
@@ -37,9 +51,9 @@ def insert_menu():
             cur.executescript(f.read())
         db.commit()
         db.close()
-        print('Success insert')
+        logger.info('Success insert')
     except BaseException as e:
-        print("Error insert database", e)
+        logger.exception("Error insert database", e)
 
 
 def create_table():
@@ -52,15 +66,17 @@ def create_table():
             cur.executescript(f.read())
         conn.commit()
         conn.close()
-        print("Success create table mainmenu")
+        logger.info("Success create table mainmenu")
     except BaseException as e:
-        print("Error create table", e)
+        logger.exception("Error create table", e)
 
 
 @app.route("/")
 def index():
+    logger.debug("Подключение ...")
     db = get_db_only_in_app()
     dbase = FDatabase(db)  # Создаём экземпляр класса где инициализируем connect и cursor
+    logger.debug("index.html загружена")
     return render_template("index.html", menu=dbase.getMenu())
 
 
@@ -68,12 +84,14 @@ def index():
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, "link_db"):
+        logger.debug("Stop session g.link_db")
         g.link_db.close()
 
 
 # insert_menu()
 # create_table()
 if __name__ == "__main__":
+    logger.debug("Start app")
     app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True)
 
---
+++ Flask/Video_Flask/utils/logger_config.py
@@ -0,0 +1,37 @@
+import sys
+import logging
+from logging import handlers
+
+dict_config = {
+        "version": 1,
+        "disable exiting loggers": False,
+        "formatters": {
+                    "base": {
+                        "format": "%(name)s | %(asctime)s | %(level)s | %(message)s"
+                    }
+        },
+        "handlers": {
+            "console": {
+                "class": logging.StreamHandler,
+                "level": "DEBUG",
+                "formatter": "base",
+                # "stream": sys.stdout
+            },
+            "file": {
+                "class": handlers.TimedRotatingFileHandler,
+                "when": "H",
+                "interval": 2,
+                "level": "WARNING",
+                "formatter": "base",
+                "filename": "logs/logs_warning.log",
+                "backupCount": 1,
+            }
+        },
+        "loggers" : {
+            "main":{
+                "level": "DEBUG",
+                "handlers": ["console",  "file"]
+            }
+        },
+        "filters": {}
+}
