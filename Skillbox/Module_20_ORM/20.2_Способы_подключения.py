956e609a51cfaf23b1e6193bbe425f093debe76e
Ruslan Domashevski <145841630+RuslanDom@users.noreply.github.com>
2025-06-14 11:45:13 +0300

    14.06

--- Skillbox/Module_20_ORM/20.2_Способы_подключения.py
+++ Skillbox/Module_20_ORM/20.2_Способы_подключения.py
@@ -1,7 +1,33 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, text, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-def first_part():
+
+"""
+Engine - определение
+engine = 
+create_engine("dialect[+driver]:///login:password@host/db_name[?key=value]")
+где:
+dialect - это название СУБД(mssql, postgres, mysql)
+driver - это название DBAPI(psycopg2, pyodbc)
+Пример:
+engine = 
+create_engine("mssql+pyodbc:///admin:qwerty@localhost/my_db")
+"""
+Base = declarative_base()
+
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    login = Column(String)
+
+    def __repr__(self):
+        return f"USER: id={self.id}, name={self.name}, email={self.email}, login={self.login}"
+
+def first_part(engine):
     with engine.connect() as connection:
         create_user_table = """CREATE TABLE IF not EXISTS users (
         id integer PRIMARY KEY, 
@@ -10,19 +36,35 @@ def first_part():
         login text NOT NULL)"""
         connection.execute(text(create_user_table))
 
+        # Текст запроса на вставку данных
         insert_q = """INSERT INTO users (name, email, login) VALUES ('Bogdan', 'bog@gmail.com', 'bogDOS')"""
         connection.execute(text(insert_q))
         # connection.commit()
 
-        filter_query = text("SELECT * FROM users WHERE id=:r_id")
+        # Текст запроса на получение данных
+        filter_query = text("SELECT * FROM users WHERE id=:r_id")  # =: Это параметр привязки
+        # Параметр привязки используют для того чтобы в дальнейшем передать именованный параметр
         cursor = connection.execute(filter_query, dict(r_id=1))
 
 
         result = cursor.fetchone()
         print(result)
 
+def second_part(engine):
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    some_object = User(name='ruslan', email='rus@gmail.com', login='Dad')
+    try:
+        # session.add(some_object)
+        # session.commit()
+        query = session.query(User).filter_by(name='ruslan').all()
+        print(query)
+    finally:
+        session.close()
+
 
 if __name__ == "__main__":
     engine = create_engine("sqlite:///pythonsql.db")
-    first_part()
-
+    # first_part(engine)
+    second_part(engine)
--- Skillbox/Module_20_ORM/20.3_Модели_БД.py
+++ Skillbox/Module_20_ORM/20.3_Модели_БД.py
@@ -2,12 +2,20 @@ from sqlalchemy import (Table, MetaData, create_engine, Column, Integer,
                         String, UniqueConstraint, Index)
 from sqlalchemy.exc import NoResultFound, MultipleResultsFound
 from sqlalchemy.orm import sessionmaker, registry, declarative_base
-engine = create_engine("sqlite:///pythonsql.db")
 
+# config
+engine = create_engine("sqlite:///pythonsql.db")
 Session = sessionmaker(bind=engine)
 session = Session()
-mapper_registry = registry()
+
+"""
+КЛАССИЧЕСКИЙ СТИЛЬ
+----------------------------------------------------------------------------
+"""
+# classic config
 metadata = MetaData()
+mapper_registry = registry()  # Для связывания объекта Table(сущность таблицы) и объекта User(отображения класса)
+
 
 users = Table(
     "users",
@@ -27,25 +35,16 @@ class User:
     def __repr__(self):
         return f"Name: {self.name} Email: {self.email} Login: {self.login}"
 
-    @classmethod
-    def get_all_users(cls):
-        return session.query(User).all()
-
-    @classmethod
-    def get_name_user(cls, name):
-        try:
-            name = session.query(User).filter_by(name=name).one()
-            return name
-        except NoResultFound:
-            print("Животное не существует")
-        except MultipleResultsFound:
-            print("Животных с этим именем больше одного")
-
 
 # Создание таблицы классическим способом
+
 mapper_registry.map_imperatively(User, users)
 metadata.create_all(bind=engine)
 
+"""
+ДЕКЛАРАТИВНЫЙ СТИЛЬ
+-------------------------------------------------------------------------------------------
+"""
 # Декларативный подход создания таблиц в БД
 Base = declarative_base()
 
@@ -59,6 +58,20 @@ class Animal(Base):
 
     def __repr__(self):
         return f"Name: {self.name} Age: {self.age}"
+
+    @classmethod
+    def get_all_users(cls):
+        return session.query(User).all()
+
+    @classmethod
+    def get_name_user(cls, name):
+        try:
+            name = session.query(User).filter_by(name=name).one()
+            return name
+        except NoResultFound:
+            print("Животное не существует")
+        except MultipleResultsFound:
+            print("Животных с этим именем больше одного")
 
 # Создание таблицы используя декларативный подход
 Base.metadata.create_all(bind=engine)
