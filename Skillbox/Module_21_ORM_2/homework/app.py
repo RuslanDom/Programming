fb5f90d76417bd60a2f198d80d29bfc39cfd7d78
Ruslan Domashevski <145841630+RuslanDom@users.noreply.github.com>
2025-06-28 16:41:16 +0300

    28.06

--- Skillbox/Module_21_ORM_2/homework/app.py
+++ Skillbox/Module_21_ORM_2/homework/app.py
@@ -1,15 +1,24 @@
+import os, ast
 from flask import Flask, request
 from marshmallow import ValidationError
 import datetime
-from models import Author, Student, ReceivingBook, Book, Base, engine, session
+from datetime import date
+from werkzeug.utils import secure_filename
+from models import Author, Student, ReceivingBook, Book, db
 from schemas import BookSchema, AuthorSchema, StudentSchema, ReceivedBookSchema
-from sqlalchemy import update
+from sqlalchemy import update, func
+from typing import List
+import csv
 
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
+ALLOWED_EXTENSIONS = {'csv'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+session = db.session
 
 @app.before_request
 def before_request():
-    Base.metadata.create_all(engine)
+    db.Base.metadata.create_all(db.engine)
 
 @app.route('/books', methods=['GET', 'POST'])
 def books_endpoint():
@@ -29,7 +38,7 @@ def books_endpoint():
             )
             session.add(new_book)
             session.commit()
-            return schema.dump(new_book)
+            return schema.dump(new_book), 201
         except ValidationError as e:
             return {"errors": e.messages}, 400
         finally:
@@ -53,7 +62,7 @@ def authors_endpoint():
             )
             session.add(new_author)
             session.commit()
-            return schema.dump(new_author)
+            return schema.dump(new_author), 201
         except ValidationError as e:
             return {"errors": e.messages}, 400
         finally:
@@ -82,7 +91,7 @@ def students_endpoint():
             )
             session.add(new_student)
             session.commit()
-            return schema.dump(new_student)
+            return schema.dump(new_student), 201
         except ValidationError as e:
             return {"errors": e.messages}, 400
     else:
@@ -107,7 +116,7 @@ def receiving_books_endpoint():
             book = session.query(Book).filter(Book.id == valid_data.book_id).first()
             book.count -= 1
             session.commit()
-            return schema.dump(new_record)
+            return schema.dump(new_record), 201
         except ValidationError as e:
             return {"errors": e.messages}, 400
         finally:
@@ -130,12 +139,141 @@ def receiving_books_return():
                 book = session.query(Book).filter(Book.id == return_book.book_id).first()
                 book.count += 1
                 session.commit()
-                return {"message": "Success"}
+                return return_book, 201
         finally:
             session.close()
+    else:
+        return {'message': 'Method not allowed'}, 405
 
 
-# получить кол-во оставшихся в библиотеке книг по автору (GET -входной параметр - ID автора)
+@app.route("/debtors", methods=['GET'])
+def get_debtors_endpoint():
+    schema = StudentSchema()
+    if request.method == 'GET':
+        debtors_list: List[int] = []
+        data_records = session.query(ReceivingBook).all()
+        for debtor in data_records:
+            if debtor.count_date_with_book > 1:
+                debtors_list.append(debtor.student_id)
+        debtors_students = session.query(Student).filter(Student.id.in_(debtors_list)).all()
+        return schema.dump(debtors_students, many=True), 200
+    else:
+        return {'message': 'Method not allowed'}, 405
+
+
+@app.route("/books/name/<string:name>")
+def find_books_by_name(name):
+    schema = BookSchema()
+    if request.method == 'GET':
+        books = session.query(Book).filter(Book.name.like("%" + name + "%")).all()
+        return schema.dump(books, many=True), 200
+    else:
+        return {'message': 'Method not allowed'}, 405
+
+
+@app.route("/books/author_id/<int:id>", methods=['GET'])
+def find_books_by_author_id(id: int):
+    """
+    получить кол-во оставшихся в библиотеке книг по автору (GET -входной параметр - ID автора)
+    :param id: int
+    :return: Список книг
+    """
+    schema = BookSchema()
+    if request.method == "GET":
+        books = session.query(Book).filter(Book.author_id == id, Book.count > 0).all()
+        return schema.dump(books, many=True), 200
+    else:
+        return {'message': 'Method not allowed'}, 405
+
+
+
+@app.route("/books/student_id/<int:id>", methods=['GET'])
+def get_books_that_the_student_has_not_read(id: int):
+    """
+    Получить список книг, которые студент не читал, при этом другие книги этого автора студент уже брал
+    (GET - входной параметр - ID студента). Пример: Я брал книгу Льва Толстого “Война и мир”, роут должен
+    вернуть другие произведения этого автора, которые есть в библиотеке
+    """
+    schema = BookSchema()
+    if request.method == "GET":
+        student_has_read_books = session.query(ReceivingBook.book_id).filter_by(student_id=id).subquery()
+        books_authors_id = session.query(Book).filter(Book.id.in_(student_has_read_books)).all()
+        books = session.query(Book).filter(
+            Book.author_id.in_(b.author_id for b in books_authors_id),
+            Book.id.not_in(b.id for b in books_authors_id)
+            ).all()
+        return schema.dump(books, many=True), 200
+    else:
+        return {'message': 'Method not allowed'}, 405
+
+
+
+@app.route("/receiving_books/avg_books", methods=["GET"])
+def get_avg_received_books():
+    """
+    получить среднее кол-во книг, которые студенты брали в этом месяце (GET)
+    :return: float
+    """
+    received_books = session.query(func.count(ReceivingBook.book_id)).filter(func.extract("month", ReceivingBook.date_of_issue).label("month") == date.today().month).all()
+    student = session.query(func.count(ReceivingBook.student_id)).group_by(ReceivingBook.student_id).all()
+    return {"average_count": received_books[0][0] / student[0][0]}, 200
+
+
+# получить самую популярную книгу среди студентов, у которых средний балл больше 4.0 (GET)
+@app.route("/books/popular", methods=["GET"])
+def the_most_popular_book():
+    schema = BookSchema()
+    if request.method == "GET":
+        # sub_request = (session.query(ReceivingBook.book_id, func.count(ReceivingBook.book_id).label("ccc")).join(ReceivingBook.students).
+        #                filter(Student.average_score > 4.0).group_by(ReceivingBook.book_id).subquery())
+        # result = session.query(Book).join(sub_request).filter(sub_request.c.book_id == Book.id).order_by(sub_request.c.ccc.desc()).first()
+        result = (session.query(Book).join(ReceivingBook.students).join(ReceivingBook.books).
+                  filter(Student.average_score > 4.0).group_by(ReceivingBook.book_id).
+                  order_by(func.count(ReceivingBook.book_id).desc()).first())
+        return schema.dump(result), 200
+    else:
+        return {"message": "Method not allowed"}, 405
+
+
+# получить ТОП-10 самых читающих студентов в этом году (GET)
+@app.route("/students/top", methods=["GET"])
+def top_students():
+    schema = StudentSchema()
+    if request.method == "GET":
+        students = (session.query(Student).join(ReceivingBook.students).group_by(ReceivingBook.student_id).
+                    order_by(func.count(ReceivingBook.student_id).desc()).limit(10).all())
+        return schema.dump(students, many=True), 200
+    else:
+        return {"message": "Method not allowed"}, 405
+
+# 3. Создайте роут, который будет принимать csv-файл с данными по студентам (разделитель ;). Используя
+# csv.DictReader (https://docs.python.org/3/library/csv.html#csv.DictReader),
+# обработайте файл и используйте Session.bulk_insert_mappings() для массовой вставки студентов.
+@app.route("/students/add_many", methods=["POST"])
+def add_many_students():
+    schema = StudentSchema()
+    if request.method == "POST":
+        data = request.files.get("filename")
+        csvfile = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(data.filename))
+        with open(csvfile, newline="", encoding="utf-8") as csv_f:
+            reader = csv.DictReader(csv_f)
+            result = []
+            for row in reader:
+                row["average_score"] = float(row["average_score"])
+                row["scholarship"] = ast.literal_eval(row["scholarship"])
+                result.append(row)
+            session.bulk_insert_mappings(Student, result)
+            session.commit()
+        students = session.query(Student).all()
+        return schema.dump(students, many=True), 200
+
+
+    else:
+        return {"message": "Method not allowed"}, 405
+
+
+# 4. По желанию. Создайте триггер на событие перед вставкой в таблицу students.
+# Триггер должен проверять, что номер телефона имеет формат +7(9**) -*** - ** - **, где * - цифра от 0 до 9.
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Skillbox/Module_21_ORM_2/homework/models.py
+++ Skillbox/Module_21_ORM_2/homework/models.py
@@ -1,16 +1,20 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, Date, Float, Boolean, Table, func
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, Date, Float, Boolean, Table, func, case
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base, backref
 from sqlalchemy.ext.associationproxy import association_proxy
 from datetime import date, datetime
 
-engine = create_engine('sqlite:///m_21_db.db')
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
+class Database:
+    def __init__(self):
+        self.engine = create_engine('sqlite:///m_21_db.db')
+        self.Base = declarative_base()
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+db = Database()
 
 
-class Book(Base):
+class Book(db.Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
@@ -24,7 +28,7 @@ class Book(Base):
     def __repr__(self):
         return f"Book(name={self.name}, count={self.count}, release_date={self.release_date}, author_id={self.author_id})"
 
-class Author(Base):
+class Author(db.Base):
     __tablename__ = 'authors'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
@@ -35,7 +39,7 @@ class Author(Base):
     def __repr__(self):
         return f"Author(name={self.name}, surname={self.surname})"
 
-class Student(Base):
+class Student(db.Base):
     __tablename__ = 'students'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
@@ -53,7 +57,7 @@ class Student(Base):
                 f"phone={self.phone}, email={self.email}, "
                 f"average_score={self.average_score}, scholarship={self.scholarship})")
 
-class ReceivingBook(Base):
+class ReceivingBook(db.Base):
     __tablename__ = 'receiving_books'
     id = Column(Integer, primary_key=True)
     book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
@@ -66,7 +70,30 @@ class ReceivingBook(Base):
 
     @hybrid_property
     def count_date_with_book(self):
-        return
+        days = self.date_of_return or datetime.now()
+        delta = (days - self.date_of_issue).days
+        return delta
+
+    @count_date_with_book.expression
+    def count_date_with_book(cls):
+        end_date = case(
+            (cls.date_of_return is None, date.today()),
+            else_=cls.date_of_return
+        )
+        return (end_date - cls.date_of_issue).days
+
+    # @hybrid_property
+    # def get_month(self):
+    #     if self.date_of_issue.month == date.today().month:
+    #         return True
+    #     return False
+    #
+    # @get_month.expression
+    # def get_month(cls):
+    #     return case(
+    #             (cls.date_of_issue. == date.today().month, True),
+    #             else_=False
+    #             )
 
     def __repr__(self):
         return (f"ReceivingBook(book_id={self.book_id}, student_id={self.student_id}, "
@@ -75,7 +102,7 @@ class ReceivingBook(Base):
 
 # Integration table
 students_books = Table('students_books',
-                       Base.metadata,
+                       db.Base.metadata,
                        Column('student_id', Integer, ForeignKey('students.id'), primary_key=True),
                        Column('book_id', Integer, ForeignKey('books.id'), primary_key=True)
                        )
@@ -111,31 +138,31 @@ def insert_data():
         Student(name="Oleg", surname="Son", phone="3-15-18", email="son@mail.us", average_score=4.5, scholarship=True),
         Student(name="Alex", surname="Krot", phone="3-26-45", email="Krot@gmail.us", average_score=3.9, scholarship=True),
     ]
-    session.add_all(authors)
-    session.add_all(students)
-    session.commit()
+    db.session.add_all(authors)
+    db.session.add_all(students)
+    db.session.commit()
 
 def received_books():
-    author_query = session.query(Author).filter(Author.surname == "Пушкин").subquery()
-    books_student_1 = session.query(Book).filter(Book.author_id == author_query.c.id).all()
-    student_1_query = session.query(Student).filter(Student.id == 1).one()
+    author_query = db.session.query(Author).filter(Author.surname == "Пушкин").subquery()
+    books_student_1 = db.session.query(Book).filter(Book.author_id == author_query.c.id).all()
+    student_1_query = db.session.query(Student).filter(Student.id == 1).one()
     for book in books_student_1:
         obj = ReceivingBook()
         obj.books = book
         obj.students = student_1_query
         obj.date_of_issue = datetime.now()
-        session.add(obj)
+        db.session.add(obj)
 
 
-    books_student_2 = session.query(Book).filter(Book.id.in_([4, 5])).all()
-    student_2_query = session.query(Student).filter(Student.average_score < 4).first()
+    books_student_2 = db.session.query(Book).filter(Book.id.in_([4, 5])).all()
+    student_2_query = db.session.query(Student).filter(Student.average_score < 4).first()
     for book in books_student_2:
         obj = ReceivingBook()
         obj.books = book
         obj.students = student_2_query
         obj.date_of_issue = datetime.now()
-        session.add(obj)
-    session.commit()
+        db.session.add(obj)
+    db.session.commit()
 
 
 # if __name__ == '__main__':
